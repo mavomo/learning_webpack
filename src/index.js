b54ade48db7c5dace2e6e6365817db55005de457
AVOMO EBOLO Michelle <michelle.avomo@cegedim.com>
2019-04-12 16:03:12 +0200

    cleaning up before output management

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 import _ from 'lodash';
-import './style.css'
-import Icon from './icon.png';
-import Data from './data/random.xml'
-
 const component = () => {
     let element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    var icon = new Image();
-    icon.src = Icon;
-    element.appendChild(icon);
-
-    console.log('------- So you want some data? ---');
-    console.log(Data);
 
     return element;
 };
