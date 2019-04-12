7d54cd08d0e31737238c9d3ce96f36c2b392d4ec
AVOMO EBOLO Michelle <michelle.avomo@cegedim.com>
2019-04-12 13:14:03 +0200

    adding font

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import _ from 'lodash';
+import './style.css'
+import Icon from './icon.png';
 
 const component = () => {
     let element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    var icon = new Image();
+    icon.src = Icon;
+    element.appendChild(icon);
+
     return element;
 };
 
