752a2e5082213a2c6fe38570158bff9b8c05c562
AVOMO EBOLO Michelle <michelle.avomo@cegedim.com>
2019-04-12 17:10:38 +0200

    inlining source map

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import _ from 'lodash';
+import printer from './printer/print'
+
 const component = () => {
     let element = document.createElement('div');
-
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    const clickableBtn = createAClickableButton();
 
+    element.appendChild(clickableBtn);
     return element;
 };
+
+function createAClickableButton() {
+    var btnElement = document.createElement('button');
+    btnElement.innerHTML = 'Click me and check the console';
+
+    btnElement.onclick = printer;
+    return btnElement;
+}
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    mode:'development',
+    entry: {
+        app: './src/index.js',
+        print: './src/printer/print.js'
+    },
+    devtool: "inline-source-map",
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: "Output management"
+        })
+    ],
     output: {
-        filename: "bundle.js",
+        filename: "[name].bundle.js",
         path: path.resolve(__dirname, 'dist')
     }
 };
